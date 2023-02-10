323e74be82ad99dd7d24f9c3a255c894e61fb13b
Justy Oberg <justyoberg1@gmail.com>
2023-02-09 18:55:23 -0800

    Reword capitalize error

--- scripts/capitalize.js
+++ scripts/capitalize.js
@@ -3,7 +3,7 @@ const capitalize = (string) => {
   if (string[0].toUpperCase() !== string[0].toLowerCase()) {
     return string[0].toUpperCase() + string.slice(1);
   } else {
-    throw new Error("Cannot capitalize that string.");
+    throw new Error("Can only capitalize strings that begin with a letter");
   };
 
 };
